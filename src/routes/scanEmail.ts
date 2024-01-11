4afe9757c0a3bce4f2f0e931793b8cf2a2e20bc6
unknown <dunkwualex6@gmail.com>
2024-01-11 15:39:10 +0100

    changes

--- src/routes/scanEmail.ts
+++ src/routes/scanEmail.ts
@@ -2,17 +2,42 @@ import { Request, Response } from "express";
 import { readCSVFile } from "../lib/readFile";
 import { validateEmail } from "../lib/validateEmail";
 
+const fs = require('fs');
+const { promisify } = require('util');
+const appendFileAsync = promisify(fs.appendFile);
+
+const { performance } = require('perf_hooks');
+
+// ...
+
 export const scanEmails = async (req:Request, res:Response) => {
-    const filePath = `${__dirname}/public/emails.csv`;
+    
+    const startTime = performance.now();
+        
+    const filePath = `${__dirname}/public/${req.body.filename}`;
 
     const emails = await readCSVFile(filePath);
-
-    const emailExtract =  emails.map((email) => email.Email || email.email);
-
-    for (const email of emailExtract) {
-        const status =  await validateEmail(email);
+    
+    const results = [];
+    for (const emailObj of emails) {
+        const email = emailObj.Email || emailObj.email;
+        const status = await validateEmail(email);
+        results.push({ email, status });
         console.log(status, email);
     }
+    
+    // Write the results to a new file
+    const outputFilePath = `${__dirname}/public/validatedEmails.csv`;
+    
+    const outputLines = results.map(({ email, status }) => `${email},${status}`).join('\n');
+    
+    await appendFileAsync(outputFilePath, outputLines);
 
-    res.send(emailExtract);
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log(`Execution time: ${executionTime} milliseconds`);
+    
+    res.send(results);
+    
 }
